f50eef52ff05cb6529aee64afff438396d0480f4
Khaleeqrehman321 <khaleeqrehman0786@gmail.com>
2023-12-14 13:41:36 +0530

    added user data

---
+++ src/com/jsp/JavaDemo/User.java
@@ -0,0 +1,37 @@
+package com.jsp.JavaDemo;
+
+public class User {
+	
+	private int userId;
+	private String UserName;
+	private String UserEmail;
+	private String password;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getUserEmail() {
+		return UserEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		UserEmail = userEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
